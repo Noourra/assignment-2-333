eaba9761fde524f7d3c2e6be5b543288624a4133
Nouurra <202101107@stu.edu.uob.bh>
2024-11-30 09:50:34 +0300

    php 1

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UOB Students Nationality Data</title>
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css">
+
+        
+    </head>
+<body>
+    <header>
+        <h1>University of Bahrain - Student Nationalities</h1>
+    </header>
